6abf0af69e8b15093c56c36d0eded690cfe94316
theodore <theodore.chung1@gmail.com>
2025-04-09 12:43:17 +0200

    trois

--- models.py
+++ models.py
@@ -1,28 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-# Table de liaison many-to-many
-student_books = db.Table('student_books',
-    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
-    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True)
-)
-
-class Student(db.Model):
-    __tablename__ = 'students'
+class StudentBook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique=True)
-    birth_date = db.Column(db.Date)
-    enrolled = db.Column(db.Boolean, default=True)
-    books = db.relationship('Book', secondary=student_books, back_populates='students')
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    borrow_date = db.Column(db.DateTime, nullable=False)
+    return_date = db.Column(db.DateTime)
+
+    student = relationship("Student", back_populates="student_books")
+    book = relationship("Book", back_populates="student_books")
 
 class Book(db.Model):
-    __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    author = db.Column(db.String(100))
-    published_at = db.Column(db.Date)
-    students = db.relationship('Student', secondary=student_books, back_populates='books')
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    published_at = db.Column(db.DateTime)
+
+    student_books = relationship("StudentBook", back_populates="book", cascade="all, delete-orphan")
+    borrowers = relationship("Student", secondary="student_book", viewonly=True)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.DateTime)
+
+    student_books = relationship("StudentBook", back_populates="student", cascade="all, delete-orphan")
+    borrowed_books = relationship("Book", secondary="student_book", viewonly=True)
 
